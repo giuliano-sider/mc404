f732e7723089bd71dad1c5e6403774db2710514a
Giuliano Sider <giuliano.sider@gmail.com>
2015-08-26 18:30:41 -0300

    adjusted commit script to add the disassembled output from objdump

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #USAGE: ../commit.sh <name of program that is stored in the homonymous directory which contains program.s as the source.>
 echo -e "\n$1/$1" >> ~/mc404/.gitignore
-git add $1.s
+git add $1.s $1.txt
 git commit -m "added assembler program $1.s to the project"
