2edb883c7f7b0d9047826f8b44e6276a95167df0
Giuliano Sider <giuliano.sider@gmail.com>
2015-09-01 22:27:38 -0300

    updated shell script

--- gcc.sh
+++ gcc.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 file=$1
-file="${file%.*}" 
+if [ ! -d "./bin" ]; then
+    mkdir "./bin"
+fi
+file="./bin/${file%.*}"
 arm-none-eabi-gcc  -mcpu=cortex-m3 -mthumb --specs=rdimon.specs -lc -lrdimon  -g $1 -o $file 
 file $file	# exibe o tipo do executavel ($file) gerado 
 arm-none-eabi-objdump -d $file > $file.txt # "disassembla" o executavel no arquivo $file.txt a partir do rótulo "main:"
